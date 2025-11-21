3cc7b485287a804c4df7a16a2d64a2aa4da30a8d
Gilbert Mendoza <gilbertm10.2024@gmail.com>
2025-11-21 08:11:52 -0400

    se agrega menu toggle responsivo

--- js/rpg.js
+++ js/rpg.js
@@ -12,4 +12,23 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 
+    const toggleBtn = document.getElementById('toggleBtn');
+    const mainMenu = document.getElementById('main-menu');
+
+    toggleBtn.addEventListener('click', (e) => {
+        e.stopPropagation();
+        const isOpen = mainMenu.classList.toggle('show');
+        toggleBtn.textContent = isOpen ? '✖' : '☰';
+        toggleBtn.classList.toggle('rotate', isOpen);
+    });
+
+    document.addEventListener('click', (e) => {
+        if (mainMenu.classList.contains('show') && !mainMenu.contains(e.target) &&
+            e.target !== toggleBtn) {
+            mainMenu.classList.remove('show');
+            toggleBtn.textContent = '☰'
+            toggleBtn.classList.remove('rotate');
+        };
+    });
+
 });
